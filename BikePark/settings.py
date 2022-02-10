985e6e8834cb350f2bf589894813ae73b5f90be0
1ypen <fdokpy@gmail.com>
2022-02-10 20:12:16 +0600

    🔧added user activation with email #5

--- BikePark/settings.py
+++ BikePark/settings.py
@@ -145,6 +145,24 @@ REST_FRAMEWORK = {
     )
 }
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
+SITE_NAME = "BikePark"
+
+DOMAIN = os.environ.get('DOMAIN_NAME')
+
+DJOSER = {
+    'SEND_ACTIVATION_EMAIL': True,
+    'ACTIVATION_URL': '/account/activate/{uid}/{token}',
+    "PASSWORD_RESET_CONFIRM_URL": "/account/reset_password/{uid}/{token}",
+    'SERIALIZERS': {'token_create': 'account.serializers.CustomTokenCreateSerializer'}
+}
+
 CORS_ALLOW_ALL_ORIGINS = True
 
 # Activate Django-Heroku.
